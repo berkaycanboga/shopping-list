f94fa37df08977069e4626de6f46ac0e2a23eee2
Burak Saraloglu <buraksaraloglu1@gmail.com>
2022-10-17 22:58:33 +0300

    Update finding relevant crawler adapter logic

--- assets/index.js
+++ assets/index.js
@@ -13,23 +13,28 @@ app.use(function (req, res, next) {
   next();
 });
 
+const CRAWLER_ADAPTERS = {
+ "hepsiburada.com": fetchHepsiburadaProductMetaData,
+ "trendyol.com": fetchTrendyolProductMetaData,
+}
+
 app.post("/createProductMeta", async (req, res) => {
   if (!req.body.source) {
     return res.status(400).send({ error: "Source is required" });
   }
   
   const sourceUrl = new URL(req.body.source);
-
-  if (sourceUrl?.hostname?.includes("hepsiburada.com")) {
-    const productMetaData = await fetchHepsiburadaProductMetaData(req.body?.source);
-    return res.send(productMetaData);
-
-  } else if (sourceUrl?.hostname?.includes("trendyol.com")) {
-    const productMetaData = await fetchTrendyolProductMetaData(req.body?.source);
-    return res.send(productMetaData);
-
-  } else {
+  
+  if (!Object.keys(CRAWLER_ADAPTERS).some(key => key === sourceUrl?.hostname)) {
     return res.status(422).send({ error: "Invalid source" });
+  }
+  
+  const adapter = CRAWLER_ADAPTERS[sourceUrl.hostname];
+  
+  try {
+    return res.send(await adapter(sourceUrl));
+  } catch (e) {
+    return res.status(400).send({ error: e.message });
   }
 });
 
